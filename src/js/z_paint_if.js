37ea57d5fb192646b6d6dbff64bbb66933d7f745
patriciapallares <patriciapallaresgonzalez@gmail.com>
2022-04-11 13:05:19 +0200

    added comments on paint_if.js

--- src/js/z_paint_if.js
+++ src/js/z_paint_if.js
@@ -1,5 +1,9 @@
+// función de condición para estilos
+  // compruebo si la largaria de favArray es igual a cero
+    // si sí es igual: añado la clase hidden a la sección fav 
+    // si no es igual: quito la clase hidden a la sección fav 
+
 function paintFavIf(){
-  console.log(favArray.length);
   if (favArray.length === 0){
    favSection.classList.add('hidden');
  }else{
