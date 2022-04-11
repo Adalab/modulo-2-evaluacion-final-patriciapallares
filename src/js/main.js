ff8ca8f2c60c363474a79c0cc4aac86d7d78b5ca
patriciapallares <patriciapallaresgonzalez@gmail.com>
2022-04-11 12:20:16 +0200

    added comments on main.js

--- src/js/main.js
+++ src/js/main.js
@@ -13,11 +13,16 @@ let notFavHtml = '';
 
 
 // función manejadora del evento click sobre las bebidas
-  //
-function handleClickDrinks(event) {
-  // 3.1.1 Color de fondo y de fuente cambian.
+  // busco el objeto de searchArray que coincide con el id del clickado
+  // compruebo si está o no en favArray
+    // si no está: push + añade clase fav
+    // si sí está: splice + retira clase fav
+  // ejecuta:
+    // función para pintar fav
+    // función para guardar en LS
+    // función de condición para estilos
 
-  event.currentTarget.classList.add('fav_drink');
+function handleClickDrinks(event) {
   // 3.1.2 Listado de favoritos en la parte izq de la pantalla, bajo el input de búsqueda
   const idDrinkSelected = event.currentTarget.id;
   const drinkFound = searchArray.find(
@@ -27,6 +32,7 @@ function handleClickDrinks(event) {
     (item) => item.drinkId === idDrinkSelected
   );
   if (favDrinkFoundI === -1) {
+    // 3.1.1 Color de fondo y de fuente cambian.
     favArray.push(drinkFound);
     event.currentTarget.classList.add('fav_drink');
   } else {
@@ -39,26 +45,35 @@ function handleClickDrinks(event) {
   paintFavIf();
 };
 
+
+// función para escuchar el evento click sobre las bebidas
+  // selecciono todas las bebidas con SelectorAll
+  // bucle con addEventListener por si se produce el click
+  // findIndex para saber si el ID de drink está también en favArray
+    // compruebo si está o no en favArray
+      // si sí está: añade clase fav
+
 // 3.1 Click sobre un cocktail
 function listenerDrinkItem() {
-  // console.log(searchArray);
   const liDrinks = document.querySelectorAll('.js_drink_search_item');
   for (const drink of liDrinks) {
     drink.addEventListener('click', handleClickDrinks);
-
-    // que me mantenga los estilos de fav en siguientes búsquedas
-    // supongo que esto podría estar en una función fuera, vamos a ver si funciona
     const favIndex = favArray.findIndex((fav) => {
       return fav.drinkId === drink.id;
     });
-    // console.log(favIndex);
-
     if (favIndex !== -1) {
       drink.classList.add('fav_drink');
     };
   };
 };
 
+// función manejadora del evento click sobre el botón buscar
+  // preventDefault para evitar que se cargue la página de nuevo
+  // ejecuto:
+    // función fetch para traer la info de la API
+    // función de condición para estilos
+  // quito la clase hidden a las secciones search y fav
+  // elimino el valor que la usuaria ha escrito en el input
 
 function handleSearchClick(event) {
   event.preventDefault();
@@ -66,14 +81,13 @@ function handleSearchClick(event) {
   searchSection.classList.remove('hidden');
   favSection.classList.remove('hidden');
   paintFavIf();
-  // así la user no tiene que borrar el campo cada vez que escriba algo nuevo
   searchInput.value = ''
 };
 
+// ejecuto getFavLS al cargar la página
+
 getFavLocalStorage();
 
-
-//events
-
+// evento click sobre el botón buscar
 
 searchBtn.addEventListener('click', handleSearchClick);
