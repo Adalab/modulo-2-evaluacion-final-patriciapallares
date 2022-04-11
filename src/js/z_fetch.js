a9b66dfd970f269999ad1747ec7f9d43856da1b6
patriciapallares <patriciapallaresgonzalez@gmail.com>
2022-04-11 12:42:23 +0200

    added comments on fetch.js

--- src/js/z_fetch.js
+++ src/js/z_fetch.js
@@ -1,13 +1,22 @@
+// función fetch para traer la info de la API
+  // recojo el valor que la usuaria ha escrito en el input
+  // declaro la URL de la API interpolando la info del input
+  // realizo el fetch
+    // then para tener la info en json
+    // then para hacer map en searchArray y crear nuevos objetos siguiendo la estructura de newDrink
+  // ejecuto:
+    // función para pintar search
+
+
+
 // 2.1 Click en buscar -> conexión al API de bebidas.
 function fetchFunction() {
   // 2.2 Recoger el texto de la usuaria para construir la URL de búsqueda
   const drink = searchInput.value;
   const apiURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`;
-  //fetch drinks from server
   fetch(apiURL)
     .then((response) => response.json())
     .then((data) => {
-      // copié y pegué de la sesión de dayana, cambiando los parámetros y las claves
       searchArray = data.drinks.map((drink) => {
         const newDrink = {
           thumbnail: drink.strDrinkThumb,
@@ -16,8 +25,6 @@ function fetchFunction() {
         };
         return newDrink;
       });
-      // no me las pintaba dentro de la función handleSearchClick pero sí aquí.
       renderDrinksList();
-      // paintSearchDrinks();
     });
 };
