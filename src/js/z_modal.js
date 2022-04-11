0267629fde0bcc01b729424715a025792d8c9291
patriciapallares <patriciapallaresgonzalez@gmail.com>
2022-04-11 12:56:22 +0200

    added comments on modal.js

--- src/js/z_modal.js
+++ src/js/z_modal.js
@@ -1,13 +1,22 @@
 // 7.1 Bebe con precaución: Modal moral
 
+// ventana modal con mensaje y botón de cerrar
+  // constantes
+  // función para mostrar ventana
+  // fucnión para cerrar ventana
+  // evento click sobre el botón cerrar
+
 const modal = document.querySelector('.js_modal');
 const closeBtn = document.querySelector('.js_close_btn');
 
 const showModal = () => {
   modal.classList.remove('hidden');
 };
+
 const closeModal = () => {
   modal.classList.add('hidden');
 };
 // setTimeout(showModal, 5000);
+
+
 closeBtn.addEventListener('click', closeModal);
